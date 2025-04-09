af32395f22238eaeb096a1f72f368dabdfc21c79
Ashley Van Steenacker <ashley@art-toys.be>
2025-04-09 14:28:57 +0200

    broken link fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: 'Vivalyte', // Usually your GitHub org/user name.
   projectName: 'docs.vivalyte.com', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
